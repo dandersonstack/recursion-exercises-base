336ec62e72f947435ccd4195b8096738327a2404
dandersonstack <danieltanderson@berkeley.edu>
2017-05-11 20:49:05 -0700

    all tests pass for jsoninator

--- jsoninator.js
+++ jsoninator.js
@@ -7,8 +7,41 @@ const _ = require('underscore'); // the real one! :)
 
 
 const stringify = function(obj) {
-  // your code goes here
+  if(obj == null) {
+    return 'null';
+  } else if(typeof(obj) === "boolean" || typeof(obj) === "number") {
+    return obj.toString();
+  } else if(typeof(obj) === "string") {
+    return "\"" + obj + "\"";
+  } else if(Array.isArray(obj)) {
+    return printArray(obj);
+  } else {
+    return printObject(obj);
+  }
+       
 };
+const printObject = function(obj) {
+  let stringBuilder = "{";
+  const arr = [];
+  _.each(obj, (item, key) => {
+    keyValuePair = stringify(key) + ":" + stringify(item);
+    arr.push(keyValuePair);
+  });
+  stringBuilder += arr.join(",");
+  stringBuilder += "}"
+  return stringBuilder;
+}
+
+const printArray = function(obj) {
+  let stringBuilder = "[";
+  const arr = [];
+  _.each(obj, item => {
+    arr.push(stringify(item));
+  });
+  stringBuilder += arr.join(",");
+  stringBuilder += "]"
+  return stringBuilder;
+}
 
 module.exports = {
   stringify: stringify

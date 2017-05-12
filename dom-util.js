177610442d5314fe71d4bf3da39ac1e15c8f33dd
dandersonstack <danieltanderson@berkeley.edu>
2017-05-11 22:49:49 -0700

    I found a typo, where I wasn't actually going through the list

--- dom-util.js
+++ dom-util.js
@@ -2,11 +2,14 @@
 const visitAllNodes = function(node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
   // Your code here
+
   callback(node);
-  //call visitAllNodes on all the children
   const childrenList = node.childNodes;
-  for(let index = 0; index < childrenList.length; index++) {
-    visitAllNodes(childrenList[0], callback);
+  if(childrenList) {
+    //call visitAllNodes on all the children
+    for(let index = 0; index < childrenList.length; index++) {
+      visitAllNodes(childrenList[index], callback);
+    }
   }
 };
 

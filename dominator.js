f3bd753518839a66302ccb687698a4aaa03ef7ba
dandersonstack <danieltanderson@berkeley.edu>
2017-05-11 23:12:28 -0700

    completed dominator, poorly written at first

--- dominator.js
+++ dominator.js
@@ -7,15 +7,32 @@ const getElementById = function(root, id) {
     if(treeArray[i].id === id) {
       return treeArray[i];
     }
-  } return -1;
+  }
 };
 
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  const elemnts = [];
+  const treeArray = flattenTreeToArray(root);
+  for(let i = 0; i < treeArray.length; i++) {
+    if(treeArray[i].className) {
+      const classNames = treeArray[i].className.toString().split(" ");
+      for(let j = 0; j < classNames.length; j++) {
+        if(classNames[j] === className) {
+          elemnts.push(treeArray[i]);
+        }
+      }
+    }
+  } return elemnts;
 };
 
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  const elemnts = [];
+  const treeArray = flattenTreeToArray(root);
+  for(let i = 0; i < treeArray.length; i++) {
+    if(treeArray[i].tagName === tagName) {
+      elemnts.push(treeArray[i]);
+    }
+  } return elemnts;
 };
 
 module.exports = {
